364bc84b9976002658a9197ad7167414f9728799
wxamlzm <zd5024@163.com>
2023-01-02 00:43:14 +0800

    UPDATE：清空style-resources-loader

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
+/*
+ * @Author: zd
+ * @Date: 2023-01-01 00:12:05
+ * @LastEditors: zd
+ * @LastEditTime: 2023-01-02 00:37:29
+ * @Description:
+ */
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  transpileDependencies: true,
-
-  pluginOptions: {
-    'style-resources-loader': {
-      preProcessor: 'scss',
-      patterns: []
-    }
-  }
+  transpileDependencies: true
 })
