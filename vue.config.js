bcafc88f006d61f819ab3d9290ee038b75d96481
wxamlzm <zd5024@163.com>
2023-01-02 00:43:32 +0800

    UPDATE：添加loader预加载方法

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,19 @@
  * @Author: zd
  * @Date: 2023-01-01 00:12:05
  * @LastEditors: zd
- * @LastEditTime: 2023-01-02 00:37:29
+ * @LastEditTime: 2023-01-02 00:43:04
  * @Description:
  */
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "~@/assets/reset.scss";`
+      }
+    }
+  }
 })
