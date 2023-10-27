ca920b577ea325ffa48432c8b440bf45da79f886
wxamlzm <zd5024@163.com>
2023-10-27 15:37:06 +0800

    UPDATE：路由规范化

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@
  * @Author: zd
  * @Date: 2022-12-31 18:47:44
  * @LastEditors: zd
- * @LastEditTime: 2023-03-03 22:43:46
+ * @LastEditTime: 2023-10-27 15:33:51
  * @Description: 路由
  */
 import Vue from 'vue'
@@ -24,13 +24,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ '../views/LoginView.vue')
+      import(/* webpackChunkName: "LoginView" */ '../views/LoginView.vue')
   },
   {
     path: '/threeThousandEmpire',
-    name: 'threeThousandEmpire',
+    name: 'ThreeThousandEmpire',
     component: () =>
-      import(/* webpackChunkName: "about" */ '../views/ThreeThousandEmpire.vue')
+      import(/* webpackChunkName: "ThreeThousandEmpire" */ '../views/ThreeThousandEmpire.vue')
   }
 ]
 
